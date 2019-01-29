7dfb5f264965302d7da1f60cde6a799260ddeea1
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-01-29 16:26:41 -0500

    added a POP UPgit add .

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+function popup(mylink, windowname) { 
+    if (! window.focus)return true;
+    var href;
+    if (typeof(mylink) == 'string') href=mylink;
+    else href=mylink.href; 
+    window.open(href, windowname, 'width=600,height=600,scrollbars=no'); 
+    return false; 
+  }
+
+  
